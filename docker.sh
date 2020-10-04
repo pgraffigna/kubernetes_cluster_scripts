405db009d9a03e06d7b80559288c8144bb6f1971
pgraffigna <69170202+pgraffigna@users.noreply.github.com>
2020-10-04 12:06:12 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
+GPG=https://download.docker.com/linux/ubuntu/gpg
+URL=https://download.docker.com/linux/ubuntu
 
-echo "++ Eliminar otras versiones de Docker"
-sudo apt-get remove docker docker-engine docker.io containerd runc
 
 echo "++ Actualiza la cache de los repos ++"
-sudo apt-get update
+sudo apt-get -qq update
 
 echo "++ Instalación de dependencias ++"
-sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent  software-properties-common
+sudo apt-get install -yq apt-transport-https ca-certificates curl gnupg-agent  software-properties-common
 
 echo "++ Agrega la llave GPG de Docker ++"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL  $GPG | sudo apt-key add -
 
 echo "++ Agrega el repositorio stable ++"
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=amd64] $URL \
    $(lsb_release -cs) \
    stable"
 
 echo "++ Actualiza la cache de los repos ++"
-sudo apt-get update
+sudo apt-get -qq update
 
-echo "++ Instala la version 18.06.1 de Docker ++" 
-sudo apt-get install -y docker-ce=18.06.1~ce~3-0~ubuntu
+echo "++ Instala Docker-ce ++" 
+sudo apt-get install -yq docker-ce
 
 echo "++ Agrega el usuario vagrant al grupo docker ++"
-usermod -aG docker vagrant
+sudo usermod -aG docker $USER
