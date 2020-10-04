0fc6cc5f87d003c84ad7b774bb167e186921cabd
pgraffigna <69170202+pgraffigna@users.noreply.github.com>
2020-10-04 12:09:38 -0300

    Update kubectl.sh

--- kubectl.sh
+++ kubectl.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
+URL=https://storage.googleapis.com/kubernetes-release/release/
 
-echo "++ Instalación de kubectl ++"
-sudo apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
-
-echo "++ Chequeo de la configuración de kubectl ++"
-kubectl cluster-info
-
-echo "++ Hacer kubectl accesible para el usuario vagrant ++"
-mkdir -p /home/vagrant/.kube
-sudo cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
-sudo chown vagrant:vagrant /home/vagrant/.kube/config
-
-echo "++ Inicio del Dashboard ++"
-kubectl proxy --address='0.0.0.0'
+curl -LOs "$URL`curl -s $URL/release/stable.txt`/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
